62bed9bacc939d6e687bbde2b8f960ae8b9e2bcb
[Abhishek Pancheshwar] <[abhishekpancheswar8@gmail.com]>
2024-07-03 10:36:26 +0530

    break

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send("hey");
+})
+
+app.listen(3000);
